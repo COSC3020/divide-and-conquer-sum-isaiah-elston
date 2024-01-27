135066fff4d3de21fe263df0f4a66ac9c93fe70b
Isaiah Elston <156853632+isaiah-elston@users.noreply.github.com>
2024-01-27 03:44:40 +0000

    Attempt #1

--- code.js
+++ code.js
@@ -1,3 +1,14 @@
 function divideAndConquerSum(a) {
-    return a;
+    if (a.length == 0) {
+        return 0;
+    } else if (a.length > 1) {
+        let netSum = 0;
+        let firstThird = Math.floor(a.length * (1/3));
+        let finalThird = Math.floor(a.length * (2/3));
+        return netSum += divideAndConquerSum(a.slice(0, firstThird--)) +
+                         divideAndConquerSum(a.slice(firstThird, finalThird)) +
+                         divideAndConquerSum(a.slice(finalThird++, a.length));
+    } else if (a.length == 1) {
+        return a[0];
+    }
 }
